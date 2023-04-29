7a53d94c9e25fbd0f221766c0200ea0f224d75a5
vicky7678 <vicky@hiehq.com>
2023-04-29 17:29:34 +0530

    Add restaurant app file

--- App.js
+++ App.js
@@ -1,42 +1,187 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Flow  => (React.createElement =>Js Object => HTML Element(render))
-// This is core react element for create react element
-const App = React.createElement("h1", { id: "heading" }, "Namaste React");
+/**
+ * Header
+ * -logo
+ * -nav-item
+ * Body
+ * -search
+ * -restaurant container
+ * --restaurant card
+ * Footer
+ * -Links
+ * -copyright
+ * -Address
+ * -contact
+ */
 
-// jsx => javascript syntax which is easier to create react element
-// jsx is not a part of react. It is used to easily create the react element for developers.
-// jsx is not html inside js. It is only html like syntax or like(xml) syntax.
-// this is JSX for create react element
-// jsx (transpile this code before it reaches the JS Engine) and transpiled by the Parcel package - Bable(convert JSX into HTML )
-// JSX (code) => Parcel(Transpiled before the JS) => Babel(convert JSX into HTML) => React.createElement => JS Object => HTML Element(render)
-// Flow => (JSX => React.createElement => JS Object => HTML Element(render))
-// Wrapping JSX for multiple element in paranthesis () is basically to understand Bable to where the JSX start and where end.
-//jsx take care of the injection attack in our system
-
-// React function Component
-//component Composition is just rendering a component to an another Component.
-//crossite scripting attack prevent by the JSX which is in this {code}.
-const data = 1000;
-const Title = () => <h1>Vicky Sharma</h1>;
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    //React Fragment -> It behaves like an empty tag by React
-    //<></> empty tag by jsx this is the short hand for the React.Fragment
-    <>
-      <div className="container">
-        <h1 className="heading">Namaste React</h1>
-        <Title></Title>
-        {Title()}
-        <Title />
+    <div className="header">
+      <div className="logo">
+        <img
+          src="https://images.unsplash.com/photo-1602934445884-da0fa1c9d3b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZCUyMGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=60&q=60"
+          alt="logo"
+        />
       </div>
-      <div id="container">
-        {Title()}
-        <Title />
+      <div className="nav-link">
+        <div>Home</div>
+        <div>About Us</div>
+        <div>Contact Us</div>
+        <div>Cart</div>
       </div>
-    </>
+    </div>
   );
 };
+
+const RestaurantCard = ({ title, src, rating, address, time }) => {
+  return (
+    <div className="restaurant_card">
+      <div className="res_img">
+        <img src={src} alt="restaurant" />
+      </div>
+      <h3 className="res_title">{title}</h3>
+      <div className="res_add">{address}</div>
+      <div className="rating">{rating} stars</div>
+      <div className="res_time">{time}</div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="food_body">
+      <div className="search">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search food..."
+        />
+      </div>
+      <div className="res_container">
+        <RestaurantCard
+          title={"The Lalit"}
+          src={
+            "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=200&q=200"
+          }
+          address={"Continental"}
+          rating={"4.4"}
+          time={"40 minutes"}
+        />
+        <RestaurantCard
+          title={"Taz Hotel"}
+          src={
+            "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+          address={"Chinese"}
+          rating={"5"}
+          time={"30 minutes"}
+        />
+        <RestaurantCard
+          title={"Seven Seas"}
+          src={
+            "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=200&q=200"
+          }
+          address={"South Indian"}
+          rating={"4.5"}
+          time={"20 minutes"}
+        />
+        <RestaurantCard
+          title={"Haldiram"}
+          src={
+            "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=200&q=200"
+          }
+          address={"North Indian"}
+          rating={"5"}
+          time={"15 minutes"}
+        />
+        <RestaurantCard
+          title={"The Lalit"}
+          src={
+            "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=200&q=200"
+          }
+          address={"Continental"}
+          rating={"4.4"}
+          time={"40 minutes"}
+        />
+        <RestaurantCard
+          title={"Taz Hotel"}
+          src={
+            "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+          address={"Chinese"}
+          rating={"5"}
+          time={"30 minutes"}
+        />
+        <RestaurantCard
+          title={"Seven Seas"}
+          src={
+            "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+          }
+          address={"South Indian"}
+          rating={"4.5"}
+          time={"20 minutes"}
+        />
+        <RestaurantCard
+          title={"Haldiram"}
+          src={
+            "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=200&q=200"
+          }
+          address={"North Indian"}
+          rating={"5"}
+          time={"15 minutes"}
+        />
+        <RestaurantCard
+          title={"The Lalit"}
+          src={
+            "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+          }
+          address={"Continental"}
+          rating={"4.4"}
+          time={"40 minutes"}
+        />
+        <RestaurantCard
+          title={"Taz Hotel"}
+          src={
+            "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+          address={"Chinese"}
+          rating={"5"}
+          time={"30 minutes"}
+        />
+        <RestaurantCard
+          title={"Seven Seas"}
+          src={
+            "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=200&q=200"
+          }
+          address={"South Indian"}
+          rating={"4.5"}
+          time={"20 minutes"}
+        />
+        <RestaurantCard
+          title={"Haldiram"}
+          src={
+            "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+          }
+          address={"North Indian"}
+          rating={"5"}
+          time={"15 minutes"}
+        />
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<App />);
--- reserve.js
+++ reserve.js
@@ -24,3 +24,47 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 //root.render basically replace the existing data inside the root id element in index.js file.
 //React is a library and it can be applied on small portion of the app.
+
+//2nd component
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+
+// Flow  => (React.createElement =>Js Object => HTML Element(render))
+// This is core react element for create react element
+const App = React.createElement("h1", { id: "heading" }, "Namaste React");
+
+// jsx => javascript syntax which is easier to create react element
+// jsx is not a part of react. It is used to easily create the react element for developers.
+// jsx is not html inside js. It is only html like syntax or like(xml) syntax.
+// this is JSX for create react element
+// jsx (transpile this code before it reaches the JS Engine) and transpiled by the Parcel package - Bable(convert JSX into HTML )
+// JSX (code) => Parcel(Transpiled before the JS) => Babel(convert JSX into HTML) => React.createElement => JS Object => HTML Element(render)
+// Flow => (JSX => React.createElement => JS Object => HTML Element(render))
+// Wrapping JSX for multiple element in paranthesis () is basically to understand Bable to where the JSX start and where end.
+//jsx take care of the injection attack in our system
+
+// React function Component
+//component Composition is just rendering a component to an another Component.
+//crossite scripting attack prevent by the JSX which is in this {code}.
+const data = 1000;
+const Title = () => <h1>Vicky Sharma</h1>;
+const HeadingComponent = () => {
+  return (
+    //React Fragment -> It behaves like an empty tag by React
+    //<></> empty tag by jsx this is the short hand for the React.Fragment
+    <>
+      <div className="container">
+        <h1 className="heading">Namaste React</h1>
+        <Title></Title>
+        {Title()}
+        <Title />
+      </div>
+      <div id="container">
+        {Title()}
+        <Title />
+      </div>
+    </>
+  );
+};
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+root1.render(<HeadingComponent />);
